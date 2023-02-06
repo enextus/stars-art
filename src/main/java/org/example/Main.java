d50fd30ec6aff66a44ff2fc18c26c2814a4e3cd7
Eduard Parkhomenko <Eduard.Parkhomenko-CIC@ibm.com>
2023-02-06 17:46:43 +0100

    ztu78

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,24 @@
 package org.example;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main {
+
+    private static final Logger logger = LogManager.getLogger("Sterne Class");
+
     public static void main(String[] args) {
+
         Sterne st = new Sterne();
+
         st.printSterne();
+        st.printTime();
+
+        System.out.println("Instances count: " + Sterne.count + "\n");
+
+        logger.info("Sterne done!");
+
+
     }
 
 }

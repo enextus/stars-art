655f9d179b15d84aa3e872d29b9632b06a61d716
enextus <enextus@gmail.com>
2023-02-11 09:11:40 +0100

    NS44

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,13 @@
 package org.example;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class Main {
 
     private static final String TASK_EXECUTED_MESSAGE = "Task executed: {}";
-    private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
         Executable task = new SimpleTask();
-        task.execute();  // outputs "Simple Task executed"
 
-        logger.info(TASK_EXECUTED_MESSAGE, task.toString());
+        task.execute();  // outputs "Simple Task executed"
     }
 
 }
--- src/main/java/org/example/Sterne.java
+++ src/main/java/org/example/Sterne.java
@@ -41,5 +41,4 @@ public class Sterne {
                 + localTime.getSecond());
     }
 
-
 }

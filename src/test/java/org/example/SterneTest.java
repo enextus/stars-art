4951806588d22e5711a45f93ab655f71d1881115
enextus <enextus@gmail.com>
2023-02-11 09:18:59 +0100

    NS49

--- src/test/java/org/example/SterneTest.java
+++ src/test/java/org/example/SterneTest.java
@@ -17,7 +17,7 @@ public class SterneTest {
         System.setOut(new PrintStream(outContent));
         Sterne sterne = new Sterne();
         sterne.printSterne();
-        assertEquals("static block: count = 0\r\n            \n*          *\n**        **\n***      ***\n****    ****\n*****  *****\n************\n",
+        assertEquals("static block: count = 0\n            \n*          *\n**        **\n***      ***\n****    ****\n*****  *****\n************\n",
                 outContent.toString());
     }
 

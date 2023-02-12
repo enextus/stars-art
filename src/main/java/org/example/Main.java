556c37612933ed37d5416600f9a064202a52d237
enextus <enextus@gmail.com>
2023-02-12 19:29:59 +0100

    files 59

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,11 @@
 package org.example;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
     private static final String TASK_EXECUTED_MESSAGE = "Task executed: {}";
 
@@ -8,6 +13,9 @@ public class Main {
         Executable task = new SimpleTask();
 
         task.execute();  // outputs "Simple Task executed"
+
+        LOGGER.info(TASK_EXECUTED_MESSAGE, "SimpleTask");
+
     }
 
 }

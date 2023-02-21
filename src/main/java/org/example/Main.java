62206cf751a9f329948e03fcd393b200f9428e32
Eduard Parkhomenko <Eduard.Parkhomenko-CIC@ibm.com>
2023-02-21 12:37:52 +0100

    ztu1011

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,10 @@
 package org.example;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 
 public class Main {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
     private static final String TASK_EXECUTED_MESSAGE = "Task executed: {}";
 
     public static void main(String[] args) {
@@ -13,7 +12,7 @@ public class Main {
 
         task.execute();  // outputs "Simple Task executed"
 
-        LOGGER.info(TASK_EXECUTED_MESSAGE, "SimpleTask");
+/*        LOGGER.info(TASK_EXECUTED_MESSAGE, "SimpleTask");*/
 
     }
 

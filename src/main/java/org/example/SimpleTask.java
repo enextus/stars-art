d189c8d5a12e383578efdaf256d639ae85a67bd1
Eduard Parkhomenko <Eduard.Parkhomenko-CIC@ibm.com>
2023-02-21 13:04:15 +0100

    ztu1013

--- src/main/java/org/example/SimpleTask.java
+++ src/main/java/org/example/SimpleTask.java
@@ -1,6 +1,11 @@
 package org.example;
 
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
 public class SimpleTask implements Executable {
+
+    private static final Logger logger = LogManager.getLogger(SimpleTask.class);
 
     @Override
     public void execute() {
@@ -13,6 +18,13 @@ public class SimpleTask implements Executable {
 
         /* System.out.println("Instances count: " + Sterne.count + "\n"); */
         System.out.print("Instances count: " + Sterne.count);
+
+        logger.debug("Debug log message");
+        logger.info("Info log message");
+        logger.error("Error log message");
+        logger.warn("Warn log message");
+        logger.fatal("Fatal log message");
+        logger.trace("Trace log message");
     }
 
 }

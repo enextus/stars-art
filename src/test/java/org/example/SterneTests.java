dd6de2491b2c9e6e762b87431e57bb3c1f37f44f
enextus <enextus@gmail.com>
2023-02-26 11:40:31 +0100

    tests

---
+++ src/test/java/org/example/SterneTests.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+class SterneTests {
+	Sterne sterne = new Sterne();
+
+	@Test
+	void testPrintSterne() {
+		sterne.printSterne();
+	}
+
+	@Test
+	void testPrintTime() {
+		sterne.printTime();
+	}
+}
+
+//Generated with love by TestMe :) Please report issues and submit feature requests at: http://weirddev.com/forum#!/testme

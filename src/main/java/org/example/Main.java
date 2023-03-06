5c54ca5b3bb6efd923e1d4f1eff6ee0acf795128
enextus <enextus@gmail.com>
2023-03-06 02:42:08 +0100

    treads 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,13 +2,13 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 300; i++) {
             Thread thread = new Thread(() -> {
                 SimpleTask simpleTask = new SimpleTask();
                 simpleTask.execute();
             });
             thread.start();
-//            thread.join(); // блокируем поток до завершения SimpleTask
+            thread.join(); // блокируем поток до завершения SimpleTask
         }
     }
 
